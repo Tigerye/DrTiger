c4be04ceb056d1f3a537261c8567949249a9d6d2
yealexchen <yechen@192.168.0.92>
2020-11-12 21:14:04 +0800

    bert

--- encode_docs.py
+++ encode_docs.py
@@ -1,3 +1,13 @@
+# NOTE: First install bert-as-service via
+# $
+# $ pip install bert-serving-server
+# $ pip install bert-serving-client
+# $
+
+#in shell
+#bert-serving-start -model_dir /data/yechen/bert/chinese_L-12_H-768_A-12 -num_worker=4
+
+
 from bert_serving.client import BertClient
 
 if __name__ == '__main__':
