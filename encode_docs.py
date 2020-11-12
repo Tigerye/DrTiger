db551870ea7855271c98588a336bb47b95e0e1d9
yealexchen <yechen@localhost>
2020-11-12 22:01:17 +0800

    encode docs.

--- encode_docs.py
+++ encode_docs.py
@@ -7,25 +7,22 @@
 #in shell
 #bert-serving-start -model_dir /data/yechen/bert/chinese_L-12_H-768_A-12 -num_worker=4
 
-
+import logging
 from bert_serving.client import BertClient
 
 
 if __name__ == '__main__':
     i = 0
     bc = BertClient()
-    fout = open("test.txt",'w',encoding = 'utf-8')
+    fout = open("/data/yechen/bert/wiki.zh.vec.txt",'w',encoding = 'utf-8')
     with open('/data/yechen/bert/wiki.zh.txt') as fin:
         for v in fin:
             i = i + 1
             doc = v.strip()
             vec = bc.encode([doc])[0]
-            if (i <= 3):
-                print('doc %d: %s\n' % (i, doc))
-                print('vec %d: %s\n' % (i, vec))
-                fout.write(" ".join(str(x) for x in vec))
-                fout.write('\n')
-            else:
-                break
-            
+            fout.write(" ".join(str(x) for x in vec))
+            fout.write('\n')
+            if (i % 10000 == 0):
+                logging.info("Encode "+str(i) + " docs")
     fout.close()
+    logging.info("Finished encode "+str(i) + " docs")
