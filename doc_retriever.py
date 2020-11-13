52d76c3041b825de8c612f86cd412c36e918101f
yealexchen <yechen@localhost>
2020-11-13 12:11:36 +0800

    read docs.

--- doc_retriever.py
+++ doc_retriever.py
@@ -3,6 +3,13 @@ from termcolor import colored
 
 i = 0
 vecs = []
+docs = []
 vecs = np.loadtxt('/data/yechen/bert/wiki.zh.vec.txt', delimiter=' ')
+docs = np.load
 print('Finished load %d doc vecs' % len(vecs))
 print('vec 1: %s' % vecs[0])
+
+with open('/data/yechen/bert/wiki.zh.txt') as fin:
+    docs = fin.readlines()
+print('Finished load %d docs' % len(docs))
+print('doc 1: %s' % docs[0])
