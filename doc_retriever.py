e0ff17760498e178dc8be728997f98b45720ff52
yealexchen <yechen@localhost>
2020-11-13 11:49:25 +0800

    print.

--- doc_retriever.py
+++ doc_retriever.py
@@ -1,9 +1,8 @@
-import logging
 import numpy as np
 from termcolor import colored
 
 i = 0
 vecs = []
 vecs = np.loadtxt('/data/yechen/bert/wiki.zh.vec.txt', delimiter=' ')
-logging.info("Finished load "+str(len(vecs)) + " doc vecs")
+print('Finished load %d doc vecs' % len(vecs))
 print('vec 1: %s' % vecs[0])
