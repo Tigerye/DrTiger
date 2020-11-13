92013cb2797b82e4b83f6a5adc4c89877435ae38
yealexchen <yechen@localhost>
2020-11-13 11:36:18 +0800

    np load.

--- doc_retriever.py
+++ doc_retriever.py
@@ -3,13 +3,5 @@ from termcolor import colored
 
 i = 0
 vecs = []
-fin = open('/data/yechen/bert/wiki.zh.vec.txt','r')
-for v in fin:
-    i = i + 1
-    vecs.append(eval(v.rstrip()))
-    if (i < 3):
-        print(vecs[i-1])
-    else:
-        break
-fin.close()
+vecs = np.loadtxt('/data/yechen/bert/wiki.zh.vec.txt', delimiter=' ')
 print("vecs length: "+len(vecs))
