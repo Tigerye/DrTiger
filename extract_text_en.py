64ed26afa87f239f4d756a91087966dd9587dba6
yealexchen <yechen@localhost>
2020-11-15 11:18:48 +0800

    en wiki.

---
+++ extract_text_en.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+from gensim.corpora import WikiCorpus
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=logging.INFO)
+'''
+    extract data from wiki dumps(*articles.xml.bz2) by gensim.
+    @2019-3-26
+'''
+def help():
+    print("Usage: python extract_text.py /data/yechen/bert/wikidatawiki-20201101-pages-articles-multistream.xml.bz2 /data/yechen/bert/wiki.en.txt")
+ 
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        help()
+        sys.exit(1)
+    logging.info("running %s" % ' '.join(sys.argv))
+    inp, outp = sys.argv[1:3]
+    i = 0
+ 
+    output = open(outp, 'w',encoding='utf8')
+    wiki = WikiCorpus(inp, lemmatize=False, dictionary={})
+    for text in wiki.get_texts():
+        output.write(" ".join(text) + "\n")
+        i = i + 1
+        if (i % 10000 == 0):
+            logging.info("Save "+str(i) + " articles")
+    output.close()
+    logging.info("Finished saved "+str(i) + " articles")
