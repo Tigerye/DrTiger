794903ac8e67dac8291c40cb8e41664c129b95ac
yealexchen <yechen@localhost>
2020-11-16 17:26:13 +0800

    retriever en.

--- scripts/readme.sh
+++ scripts/readme.sh
@@ -99,5 +99,26 @@ python run_squad.py \
   "best_f1": 60.03961921531943,
   "best_f1_thresh": -0.000835418701171875
 }
+
+
+#en
+BERT_BASE_DIR=/data/yechen/bert/uncased_L-12_H-768_A-12
+SQUAD_DIR=/data/yechen/squad
+
+python run_squad.py \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --do_train=True \
+  --train_file=$SQUAD_DIR/train-v2.0.json \
+  --do_predict=True \
+  --predict_file=$SQUAD_DIR/dev-v2.0.json \
+  --train_batch_size=8 \
+  --learning_rate=3e-5 \
+  --num_train_epochs=2.0 \
+  --max_seq_length=384 \
+  --doc_stride=128 \
+  --output_dir=$SQUAD_DIR/squad_2.0_base/ \
+  --version_2_with_negative=True
   
   
