14177f447a3368b564ef88ed43c7c02c1c5f5252
yealexchen <yechen@localhost>
2020-11-24 22:15:19 +0800

    sec para index.

---
+++ build_index_section.py
@@ -0,0 +1,31 @@
+#in shell
+#bert-serving-start -model_dir /data/yechen/bert/chinese_L-12_H-768_A-12 -num_worker=8 -max_seq_len=128
+
+import logging
+import pickle
+from rank_bm25 import BM25Okapi
+import time
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=logging.INFO)
+
+print('loading docs...')
+tic = time.perf_counter()
+with open('/data/yechen/bert/wiki.en.section.txt') as fin1:
+    docs = fin1.readlines()
+toc = time.perf_counter()
+print('doc 1: %s' % docs[0])
+print(f"Finished load [%d] docs in [{toc - tic:0.2f}] seconds\n" % len(docs))
+
+print('building bm25...')
+tic = time.perf_counter()
+bm25 = BM25Okapi([doc.split(" ") for doc in docs])
+toc = time.perf_counter()
+print(f"Finished build bm25 on corpus with [{bm25.corpus_size}] documents and [{len(bm25.idf)}] vocabulary in [{toc - tic:0.2f}] seconds\n")
+
+print('dumping bm25...')
+tic = time.perf_counter()
+with open("/data/yechen/bert/drtiger/bm25_en_section","wb") as fout:
+    pickle.dump(bm25, fout)
+toc = time.perf_counter()
+print(f"Finished dump bm25 index in [{toc - tic:0.2f}] seconds\n")
+        
+    
