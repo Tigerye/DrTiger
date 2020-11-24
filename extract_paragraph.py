cf9d6e35a541b80ef424995c8f3951e18b3f5dae
yealexchen <yechen@localhost>
2020-11-24 17:33:04 +0800

    numsec

--- extract_paragraph.py
+++ extract_paragraph.py
@@ -5,11 +5,13 @@ import json
 output = open('/data/yechen/bert/wiki.en.para.txt', 'w',encoding='utf8')
 with utils.smart_open('/data/yechen/bert/enwiki-20201101-pages-articles-multistream1.xml-p1p41242.json.gz', 'rb') as f:
     numart = 0
+    numsec = 0
     numpar = 0
     for line in f:
         numart = numart+1
         article = json.loads(line)
         for section_text in article['section_texts']:
+            numsec = numsec+1
             para_texts = section_text.strip('\n')
             for para_text in para_texts:
                 numpar = numpar+1
@@ -17,4 +19,4 @@ with utils.smart_open('/data/yechen/bert/enwiki-20201101-pages-articles-multistr
         if (numart > 2):
             break
 output.close()
-print(f"extracted [{numart}] articles and [{numpar}] paragraphs.\n")
+print(f"extracted [{numart}] articles, [{numsec}] sections and [{numpar}] paragraphs.\n")
