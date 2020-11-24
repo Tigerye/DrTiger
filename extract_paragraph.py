5dbf64991cf417f87d2449e0d722dcc5b8bc5df3
yealexchen <yechen@localhost>
2020-11-24 18:41:50 +0800

    loogging

--- extract_paragraph.py
+++ extract_paragraph.py
@@ -1,6 +1,8 @@
 from gensim import utils
 import json
 import re
+import logging
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=logging.INFO)
 
 # iterate over the plain text data we just created
 output = open('/data/yechen/bert/wiki.en.para.txt', 'w',encoding='utf8')
@@ -20,5 +22,9 @@ with utils.smart_open('/data/yechen/bert/enwiki-20201101-pages-articles-multistr
                 if text:
                     numpar = numpar+1
                     output.write(text+"\n")
+        
+        if (numart % 10000 == 0):
+            logging.info("extracted "+str(numart) + " articles")
 output.close()
 print(f"extracted [{numart}] articles, [{numsec}] sections and [{numpar}] paragraphs.\n")
+logging.info("Finished extrqct "+str(numart) + " articles")
