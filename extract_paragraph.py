8d981f6c82ee852e9b568c9db2ef85d63cd208c0
yealexchen <yechen@localhost>
2020-11-24 16:56:02 +0800

    smart open.

--- extract_paragraph.py
+++ extract_paragraph.py
@@ -3,7 +3,7 @@ import json
 
 # iterate over the plain text data we just created
 output = open('/data/yechen/bert/wiki.en.para.txt', 'w',encoding='utf8')
-with utils.smart_open('/data/yechen/bert/enwiki-20201101-pages-articles-multistream1.xml-p1p41242.json', 'rb') as f:
+with utils.smart_open('/data/yechen/bert/enwiki-20201101-pages-articles-multistream1.xml-p1p41242.json.gz', 'rb') as f:
     numart = 0
     numpar = 0
     for line in f:
