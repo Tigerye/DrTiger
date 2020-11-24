bc3d477f07250ea6614119779b5e4aec3f1da449
yealexchen <yechen@localhost>
2020-11-24 19:45:17 +0800

    full run

--- extract_section.py
+++ extract_section.py
@@ -5,8 +5,8 @@ import logging
 logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=logging.INFO)
 
 # iterate over the plain text data we just created
-output = open('/data/yechen/bert/wiki.en.sec.txt', 'w',encoding='utf8')
-with utils.smart_open('/data/yechen/bert/enwiki-20201101-pages-articles-multistream1.xml-p1p41242.json.gz', 'rb') as f:
+output = open('/data/yechen/bert/wiki.en.section.txt', 'w',encoding='utf8')
+with utils.smart_open('/data/yechen/bert/enwiki-20201101-pages-articles-multistream.json.gz', 'rb') as f:
     numart = 0
     numsec = 0
     for line in f:
