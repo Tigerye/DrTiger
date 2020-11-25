4d00e9e14ef51c77858f521875f130c6ee31f5d0
yealexchen <yechen@localhost>
2020-11-25 13:52:19 +0800

    exclude sections

--- extract_paragraph.py
+++ extract_paragraph.py
@@ -6,6 +6,7 @@ logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=logg
 
 # iterate over the plain text data we just created
 #output = open('/data/yechen/bert/wiki.en.paragraph.txt', 'w',encoding='utf8')
+exclude_sections = {'See also', 'References', 'Further reading', 'External links'}
 output = open('/data/yechen/bert/wiki.en.para.txt', 'w',encoding='utf8')
 #with utils.smart_open('/data/yechen/bert/enwiki-20201101-pages-articles-multistream.json.gz', 'rb') as f:
 with utils.smart_open('/data/yechen/bert/enwiki-20201101-pages-articles-multistream1.xml-p1p41242.json.gz', 'rb') as f:
@@ -17,7 +18,7 @@ with utils.smart_open('/data/yechen/bert/enwiki-20201101-pages-articles-multistr
         article = json.loads(line)
         section_titles = article['section_titles']
         for section_text in article['section_texts']:
-            if (section_titles[numsec] == "External links"):
+            if (section_titles[numsec] in exclude_sections):
                 continue
             numsec = numsec+1
             section_text = section_text.replace("\n*","\n").strip('\n')
