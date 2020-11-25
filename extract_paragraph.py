08d99b8fc0e623c69b934fe30ad7d1ecd8e81186
yealexchen <yechen@localhost>
2020-11-25 14:03:12 +0800

    exclude sect.

--- extract_paragraph.py
+++ extract_paragraph.py
@@ -6,7 +6,7 @@ logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=logg
 
 # iterate over the plain text data we just created
 #output = open('/data/yechen/bert/wiki.en.paragraph.txt', 'w',encoding='utf8')
-exclude_sections = {'See also', 'References', 'Further reading', 'External links'}
+exclude_sections = {'See also', 'References', 'Further reading', 'External links', 'Sources', 'Bibliography'}
 output = open('/data/yechen/bert/wiki.en.para.txt', 'w',encoding='utf8')
 #with utils.smart_open('/data/yechen/bert/enwiki-20201101-pages-articles-multistream.json.gz', 'rb') as f:
 with utils.smart_open('/data/yechen/bert/enwiki-20201101-pages-articles-multistream1.xml-p1p41242.json.gz', 'rb') as f:
