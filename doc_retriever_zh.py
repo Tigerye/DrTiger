fdb317e9bcaff2e68bd737f697c8cd8519219e81
yealexchen <yechen@localhost>
2020-11-27 18:31:05 +0800

    wrong import.

--- doc_retriever_zh.py
+++ doc_retriever_zh.py
@@ -11,7 +11,6 @@ from rank_bm25 import BM25Plus as BM25
 import time
 import json
 import os
-from doc_retriever import tokquery
 logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=logging.INFO)
 
 def help():
