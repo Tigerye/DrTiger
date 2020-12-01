5c623139c9ceeaba0fc27f0bb6d9d54e31ff1dc5
yealexchen <yechen@localhost>
2020-12-01 16:08:47 +0800

    combine squad train

--- combine_squad_data.py
+++ combine_squad_data.py
@@ -4,14 +4,32 @@ import logging
 logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', level=logging.INFO)
 
 def help():
-    print("Usage: python combine_squad_data.py /data/yechen/squad/train-v2.0.json /data/yechen/squad/newsqa/newsqa_to_squad_train.json /data/yechen/squad/squad-and-newsqa-train-v2.0.json")
+    print("Usage: python combine_squad_data.py")
+data_dir = '/data/yechen/squad/data/'
+infiles = {data_dir+'train-v2.0.json',
+           data_dir+'newsqa_to_squad_train.json',
+           data_dir+'triviaqa_to_squad_wikipedia_train.json',
+           data_dir+'triviaqa_to_squad_web_train.json',
+           data_dir+'qangaroo_to_squad_wikihop_train.json',
+           data_dir+'qangaroo_to_squad_medhop_train.json',
+           data_dir+'mctest_to_squad_mc500.train.json',
+           data_dir+'mctest_to_squad_mc160.train.json'
+           }
+outfile = data_dir+'combined-squad-train-v2.0.json'
+
+# infiles = {data_dir+'dev-v2.0.json',
+#            data_dir+'newsqa_to_squad_dev.json',
+#            data_dir+'triviaqa_to_squad_wikipedia_dev.json',
+#            data_dir+'triviaqa_to_squad_web_dev.json',
+#            data_dir+'qangaroo_to_squad_wikihop_dev.json',
+#            data_dir+'qangaroo_to_squad_medhop_dev.json',
+#            data_dir+'mctest_to_squad_mc500.dev.json',
+#            data_dir+'mctest_to_squad_mc160.dev.json'
+#            }
+# outfile = data_dir+'combined-squad-dev-v2.0.json'
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        help()
-        sys.exit(1)
     logging.info("running %s" % ' '.join(sys.argv))
-    infile1, infile2, outfile = sys.argv[1:4]
     
     data = {
             "version": "v2.0",
@@ -21,17 +39,13 @@ if __name__ == '__main__':
             }
     
     i = 0
-    with open(infile1) as f:
-        d = json.load(f)
-    for doc in d["data"]:
-        data['data'].append(doc)
-        i+=1
-    
-    with open(infile2) as f:
-        d = json.load(f)
-    for doc in d["data"]:
-        data['data'].append(doc)
-        i+=1
+    for infile in infiles:
+        with open(infile) as f:
+            d = json.load(f)
+        for doc in d["data"]:
+            data['data'].append(doc)
+            i+=1
+        print(f"combined file: [{infile}]")
     
     with open(outfile, 'w') as f:
         json.dump(data, f)
