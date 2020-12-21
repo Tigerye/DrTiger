33ab73d852e3e47d9938f156855411a085949117
yealexchen <yechen@localhost>
2020-12-21 11:59:43 +0800

    gen news

--- scripts/generate-news-doc.sh
+++ scripts/generate-news-doc.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+outfile=/mnt/disk2/data1/news/datap/out-txt-v2/cn/2020/news.txt
 while read line
 do
-filename=$(echo $line | sed 's/.*\s//g');
-filesize=$(echo $line | sed 's/\s.*//g');
-echo filename: $filename
-echo filesize: $filesize
+
+filename=$(echo $line | sed 's/.*\s//g')
+filesize=$(echo $line | sed 's/\s.*//g')
+
+if [[ $filesize -gt 5 ]]
+then
+	head -1 $filename >> $outfile
+fi
+
 done < /mnt/disk2/data1/news/datap/out-txt-v2/cn/2020/news-2020-zh.list
