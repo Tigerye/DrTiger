d76651343b3fa9eab5ac508af9edb07d7eb4d1c6
yealexchen <yechen@localhost>
2020-12-29 20:29:08 +0800

    red doc

--- doc_retriever_zh.py
+++ doc_retriever_zh.py
@@ -107,7 +107,7 @@ if __name__ == '__main__':
         for docidx, bm25 in enumerate(bm25s):
             scores = bm25.get_scores(tokquery)
             topk_idx = np.argsort(scores)[::-1][:topk]
-            print('retrieved top [%d] docs from [%s] similar to "%s":' % (topk, colored(docfiles[docidx], 'green'), colored(query, 'green')))
+            print('retrieved top [%d] docs from [%s] similar to "%s":' % (topk, colored(docfiles[docidx], 'red'), colored(query, 'green')))
             for idx in topk_idx:
                 reader_docs.append(docs[docs_offset[docidx]+idx])
                 reader_scores.append(scores[idx])
