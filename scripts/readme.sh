60594bda28a02993427d73dfe5a5ae1b6667f770
yealexchen <yechen@localhost>
2021-01-07 13:34:45 +0800

    keras opt

--- scripts/readme.sh
+++ scripts/readme.sh
@@ -2391,6 +2391,20 @@ python create_pretraining_data.py \
   --random_seed=12345 \
   --dupe_factor=5 \
   --do_whole_word_mask=True
+  
+python run_pretraining.py \
+  --input_file=$DATA_DIR/tf_examples.tfrecord \
+  --output_dir=$DATA_DIR/bert_model_sample \
+  --do_train=True \
+  --do_eval=True \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --train_batch_size=32 \
+  --max_seq_length=128 \
+  --max_predictions_per_seq=20 \
+  --num_train_steps=20 \
+  --num_warmup_steps=10 \
+  --learning_rate=2e-5
 
 
 
