30f55c261df556005edc840154618afbeb378c60
yealexchen <yechen@localhost>
2021-01-07 21:51:41 +0800

    run pre

--- scripts/readme.sh
+++ scripts/readme.sh
@@ -2395,12 +2395,39 @@ python create_pretraining_data_v2.py \
   --dupe_factor=5 \
   --do_whole_word_mask=True
   
+python create_pretraining_data.py \
+  --input_file=./sample_text.txt \
+  --output_file=$DATA_DIR/tf_examples_sample.tfrecord \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --do_lower_case=True \
+  --max_seq_length=128 \
+  --max_predictions_per_seq=20 \
+  --masked_lm_prob=0.15 \
+  --random_seed=12345 \
+  --dupe_factor=5 \
+  --do_whole_word_mask=True
+  
 python run_pretraining_v2.py \
   --input_file=$DATA_DIR/tf_examples_sample.tfrecord \
   --output_dir=$DATA_DIR/bert_model_sample \
   --do_train=True \
   --do_eval=True \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --train_batch_size=32 \
+  --max_seq_length=128 \
+  --max_predictions_per_seq=20 \
+  --num_train_steps=2000 \
+  --num_warmup_steps=100 \
+  --learning_rate=2e-5
+  
+python run_pretraining.py \
+  --input_file=$DATA_DIR/tf_examples_sample.tfrecord \
+  --output_dir=$DATA_DIR/bert_model_sample \
+  --do_train=True \
+  --do_eval=True \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
   --train_batch_size=32 \
   --max_seq_length=128 \
   --max_predictions_per_seq=20 \
