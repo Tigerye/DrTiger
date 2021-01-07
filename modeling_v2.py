1519a597ef6908ed6febf2daa5e8245c511b21ea
yealexchen <yechen@localhost>
2021-01-07 15:03:06 +0800

    tfa

--- modeling_v2.py
+++ modeling_v2.py
@@ -26,6 +26,7 @@ import re
 import numpy as np
 import six
 import tensorflow as tf
+import tensorflow_addons as tfa
 
 
 class BertConfig(object):
@@ -361,7 +362,7 @@ def dropout(input_tensor, dropout_prob):
 
 def layer_norm(input_tensor, name=None):
   """Run layer normalization on the last dimension of the tensor."""
-  return tf.keras.layers.experimental.LayerNormalization(
+  return tfa.layers.layer_norm(
       inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
 
 
