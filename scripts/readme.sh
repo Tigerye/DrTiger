739a445fe87a27edc2a6f44d94e99c1020d6e2ed
yealexchen <yechen@localhost>
2021-01-08 10:34:15 +0800

    est tpu

--- scripts/readme.sh
+++ scripts/readme.sh
@@ -2414,17 +2414,45 @@ python run_pretraining_v2.py \
   --do_eval=True \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --train_batch_size=128 \
+  --max_seq_length=128 \
+  --max_predictions_per_seq=20 \
+  --num_train_steps=10000 \
+  --num_warmup_steps=1000 \
+  --learning_rate=2e-5
+  
+python run_pretraining_v2.py \
+  --input_file=$DATA_DIR/tf_examples_sample.tfrecord \
+  --output_dir=$DATA_DIR/bert_model_sample \
+  --do_train=False \
+  --do_eval=True \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
   --train_batch_size=32 \
   --max_seq_length=128 \
   --max_predictions_per_seq=20 \
-  --num_train_steps=2000 \
-  --num_warmup_steps=100 \
+  --num_train_steps=20 \
+  --num_warmup_steps=10 \
   --learning_rate=2e-5
   
 python run_pretraining.py \
   --input_file=$DATA_DIR/tf_examples_sample.tfrecord \
   --output_dir=$DATA_DIR/bert_model_sample \
   --do_train=True \
+  --do_eval=True \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --train_batch_size=32 \
+  --max_seq_length=128 \
+  --max_predictions_per_seq=20 \
+  --num_train_steps=20 \
+  --num_warmup_steps=10 \
+  --learning_rate=2e-5
+  
+python run_pretraining.py \
+  --input_file=$DATA_DIR/tf_examples_sample.tfrecord \
+  --output_dir=$DATA_DIR/bert_model_sample \
+  --do_train=False \
   --do_eval=True \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
