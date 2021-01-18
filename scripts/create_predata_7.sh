93fdbbcb40752ddf476e12849ea679753701814a
yealexchen <yechen@localhost>
2021-01-18 18:05:02 +0800

    script 7

--- scripts/create_predata_7.sh
+++ scripts/create_predata_7.sh
@@ -25,12 +25,21 @@ python /home/yechen/Workspace/DrTiger/create_pretraining_data.py \
 
 done
 
-infile=/data/yechen/bert/news.zh.2018.pre.txt
-echo "create predata from ${infile} to tf_examples_news2018_seq512.tfrecord ..."
+echo "done create predata for year ${year}."
+
+year=2018
+
+BERT_BASE_DIR=/data/yechen/bert/chinese_L-12_H-768_A-12
+DATA_DIR=/data/yechen/bert
+
+for infile in `ls /data/yechen/bert/news.zh.${year}.txt.pre.part*.cln`;
+do
+outpart=`echo $infile | sed 's/.*pre\.//g' | sed 's/\.cln//g'`
+echo "create predata from ${infile} to tf_examples_news${year}${outpart}_seq512.tfrecord ..."
 
 python /home/yechen/Workspace/DrTiger/create_pretraining_data.py \
   --input_file=${infile} \
-  --output_file=$DATA_DIR/seq512/tf_examples_news2018_seq512.tfrecord \
+  --output_file=$DATA_DIR/seq512/tf_examples_news${year}${outpart}_seq512.tfrecord \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --do_lower_case=True \
   --max_seq_length=512 \
@@ -39,5 +48,7 @@ python /home/yechen/Workspace/DrTiger/create_pretraining_data.py \
   --random_seed=12345 \
   --dupe_factor=5 \
   --do_whole_word_mask=True
+
+done
 
 echo "done create predata for year ${year}."
