d950571dd9e91064bdf718b344e6845c64cc288f
yealexchen <yechen@localhost>
2021-02-22 11:18:12 +0800

    tok

--- run_squad_nocache.py
+++ run_squad_nocache.py
@@ -873,11 +873,12 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
         seen_predictions[final_text] = True
         
         #debug
+        print(''.join(feature.tokens))
         print(pred.start_index,pred.end_index)
         print(feature.token_to_orig_map)
         print(final_text)
-        print(' '.join(tok_tokens))
-        print(' '.join(orig_tokens))
+        print(''.join(tok_tokens))
+        print(''.join(orig_tokens))
         sys.exit()
         
       else:
